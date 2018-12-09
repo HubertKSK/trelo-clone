e2741a407490f5ada970a615bc20b7f2ec8612f4
Hubert Kowalczyk <hubertksk@gmail.com>
2018-12-09 16:34:22 +0100

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -4,53 +4,52 @@ import Loading from "./Loading";
 import FormComponent from "./FormComponent";
 
 class AppComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-      login: null,
-      passoword: null,
-      badPass: false
-    };
-  }
-  onSubmit = (newlogin, newpassword) => {
-    this.setState({
-      login: newlogin,
-      passoword: newpassword
-    })
-    if (newlogin === "admin" && newpassword === "admin") {
+   constructor(props) {
+      super(props);
+      this.state = {
+         user: null,
+         login: null,
+         passoword: null,
+         badPass: false
+      };
+   }
+   onSubmit = (newlogin, newpassword) => {
       this.setState({
-        user: "admin"
+         login: newlogin,
+         passoword: newpassword
       });
-    } else {
-      this.setState({
-        badPass: true
-      });
-    }
-  };
-  render() {
-    if (this.state.user !== null) {
-      return (
-        <div className="AppComponent">
-          <Loading />
-        </div>
-      );
-    }
-    else if (this.state.user === null && this.state.badPass === false) {
-      return (
-        <div className="AppComponent">
-          <FormComponent onSubmit={this.onSubmit} />
-        </div>
-      );
-    }
-    else if (this.state.user === null && this.state.badPass === true) {
-      return (
-        <div className="AppComponent">
-          <FormComponent onSubmit={this.onSubmit} />
-          <h3>Enter correct login details</h3>
-        </div>
-      );
-    }
-  }
+      if (this.state.login === "admin" && this.state.passoword === "admin") {
+         this.setState({
+            user: "admin"
+         });
+      } else {
+         this.setState({
+            badPass: true
+         });
+      }
+   };
+
+   render() {
+      if (this.state.user !== null) {
+         return (
+            <div className="AppComponent">
+               <Loading />
+            </div>
+         );
+      } else if (this.state.user === null && this.state.badPass === false) {
+         return (
+            <div className="AppComponent">
+               <FormComponent onSubmit={this.onSubmit} />
+            </div>
+         );
+      } else if (this.state.user === null && this.state.badPass === true) {
+         return (
+            <div className="AppComponent">
+               <FormComponent onSubmit={this.onSubmit} />
+               <h3>Enter correct login details</h3>
+            </div>
+         );
+      }
+   }
 }
 export default AppComponent;
